bc677f09cada1a7a3ba786ff5e8800028a52477a
Goga Tirkiya <tirkiya@icloud.com>
2018-11-18 17:43:59 +0100

    fix imports

--- src/main/java/jewel/api/controller/AdvertController.java
+++ src/main/java/jewel/api/controller/AdvertController.java
@@ -10,7 +10,15 @@ import jewel.repository.ArchivedAdvertRepository;
 import jewel.repository.domain.Advert;
 import jewel.repository.domain.ArchivedAdvert;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
